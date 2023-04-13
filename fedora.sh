b6f0d8044ab4a43168980c2fef68276d2bdc45d2
Alexsandro Matias <matiasalexsandro@gmail.com>
2023-04-12 21:46:58 -0300

    Adicionando o Eclipse

--- fedora.sh
+++ fedora.sh
@@ -16,7 +16,25 @@ sudo dnf install brave-browser -y
 
 
 # Instalação do Java 8
-sudo dnf install java-1.8.0-openjdk-devel 
+sudo dnf install java-1.8.0-openjdk-devel
+
+# Download e Instalação do Eclipse para JavaEE
+wget -c https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/2021-12/R/eclipse-jee-2021-12-R-linux-gtk-x86_64.tar.gz -O eclipse.tar.gz
+sudo tar -zxvf eclipse.tar.gz -C /opt/
+sudo mv /opt/eclipse*/ /opt/eclipse
+
+
+# Criação de atalho
+echo "
+[Desktop Entry]
+Name=Eclipse
+Comment=Eclipse
+Exec=/opt/eclipse/eclipse
+Icon=/opt/eclipse/icon.xpm
+Terminal=false
+Type=Application" > eclipse.desktop
+
+cp eclipse.desktop ~/.local/share/applications/
 
 
 # Instalação de programas básicos
